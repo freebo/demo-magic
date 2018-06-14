6ab96cccab8623c3f0a8dd1037c0ae6cb0593902
Ubuntu <mike@au-docker-dev.03t54t4zoq5ubckkcfc3wcau3b.px.internal.cloudapp.net>
2018-06-14 05:15:11 +0000

    scripted

--- samples/nginx-pod.sh
+++ samples/nginx-pod.sh
@@ -3,7 +3,7 @@
 ########################
 # include the magic
 ########################
-. ../demo-magic.sh 
+. ../demo-magic.sh -n
 
 
 ########################
@@ -20,11 +20,12 @@
 #
 # see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
 #
-DEMO_PROMPT="${GREEN} ${CYAN}\W "
+#DEMO_PROMPT="${GREEN} ${CYAN} [\t][\u@\h:\w]\$ "
+DEMO_PROMPT="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
 
 #remove resources if they exist already
-kubectl delete service nginx
-kubectl delete deployment nginx
+kubectl delete service nginx >/dev/null 2>&1
+kubectl delete deployment nginx >/dev/null 2>&1
 
 # hide the evidence
 clear
@@ -35,12 +36,16 @@ RG=demok8-rg
 NAME=demok8-cl
 
 pe "az aks get-credentials --resource-group $RG --name $NAME"
+wait
 
 pe "kubectl get nodes"
+wait
 
 pe "kubectl run nginx --image nginx --port 80"
+wait
 
 pe "kubectl expose deployment nginx --type LoadBalancer --port 80"
+wait
 
 pe "kubectl get service"
 
